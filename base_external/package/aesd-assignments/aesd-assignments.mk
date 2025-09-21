abc7003a5b5002765254c19f659d1f121897442d
Paul Nwoko <npail511@gmail.com>
2025-09-21 21:03:57 +0100

    assignment 4-2 submission

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 43c30385ca7bf19c8051adc758a1b7db832a15b7
+AESD_ASSIGNMENTS_VERSION = 122c20323ac214d018c0e1bf3913461ed52a6f33
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
